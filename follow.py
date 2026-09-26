15e15cfec6874e92f2515274708d0333e7e232b9
Nipun Kothari <nipunkothari04@gmail.com>
2026-09-26 19:27:18 +0530

    Added files as scaffolding for pending issues

---
+++ follow.py
@@ -0,0 +1,7 @@
+'''
+    implements:
+        - following/unfollowing an artist
+        - checking if an artist is followed 
+        - fetching new music from an artist
+        - persisting followed artists and release state as JSON
+'''
---
+++ lyrics.py
@@ -0,0 +1,7 @@
+'''
+    implements:
+        - fetching synced/plain lyrics using ytmusic_client
+        - convert the lyrics fetched as text into .lrc files
+        - save them alongside the song with the same name
+        - symlink the lyrics just like the song (if song already done)
+'''
